4ef894e78d22af31e42a27797341ae1fc213f426
xaybzceng <adithep.seesaeng@gmail.com>
2026-03-12 18:45:48 +0700

    Update onlinebot.py

--- onlinebot.py
+++ onlinebot.py
@@ -4,14 +4,14 @@ import os
 from flask import Flask
 from threading import Thread
 
-app = Flask('')
+app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Bot is running!"
+    return "Bot is alive!"
 
 def run():
-    app.run(host='0.0.0.0', port=10000)
+    app.run(host="0.0.0.0", port=10000)
 
 def keep_alive():
     t = Thread(target=run)
